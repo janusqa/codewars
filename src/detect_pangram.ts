61f5d337752edefec94927f8fc42ac2782425aa3
Janus Developer <dev@retrievo.net>
2022-10-07 13:30:53 -0400

    isPangram

---
+++ src/detect_pangram.ts
@@ -0,0 +1,24 @@
+import { assert } from 'chai';
+
+const isPangram = (phrase: string): boolean =>
+    new Set(
+        phrase
+            .toLowerCase()
+            .split('')
+            .filter(
+                (char) =>
+                    char !== ' ' &&
+                    char.charCodeAt(0) >= 97 &&
+                    char.charCodeAt(0) <= 122
+            )
+    ).size >= 26;
+
+describe('isPangram', function () {
+    it('test', function () {
+        assert.strictEqual(
+            isPangram('The quick brown fox jumps over the lazy dog.'),
+            true
+        );
+        assert.strictEqual(isPangram('This is not a pangram.'), false);
+    });
+});
