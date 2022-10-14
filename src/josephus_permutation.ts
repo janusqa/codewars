648a3e0d333917f2dd1f277ce5f1b59b1f1bb13c
Janus Developer <dev@retrievo.net>
2022-10-14 00:23:46 -0400

    Josephus Permutation

--- src/josephus_permutation.ts
+++ src/josephus_permutation.ts
@@ -2,11 +2,12 @@ import { expect } from 'chai';
 
 export const josephus = (items: any[], k: number): any[] => {
     const result: any[] = [];
-    let start = 0;
+    let sacrifice = 0;
 
     while (items.length > 0) {
-        result.push(...items.splice((start + (k - 1)) % items.length, 1));
-        start += k - 1;
+        sacrifice += k - 1;
+        sacrifice %= items.length;
+        result.push(...items.splice(sacrifice, 1));
     }
 
     return result;
@@ -17,19 +18,19 @@ describe('josephus permutation', function () {
         expect(josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)).to.eql([
             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
         ]);
-        // expect(josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).to.eql([
-        //     2, 4, 6, 8, 10, 3, 7, 1, 9, 5,
-        // ]);
-        // expect(josephus(['C', 'o', 'd', 'e', 'W', 'a', 'r', 's'], 4)).to.eql([
-        //     'e',
-        //     's',
-        //     'W',
-        //     'o',
-        //     'C',
-        //     'd',
-        //     'r',
-        //     'a',
-        // ]);
+        expect(josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).to.eql([
+            2, 4, 6, 8, 10, 3, 7, 1, 9, 5,
+        ]);
+        expect(josephus(['C', 'o', 'd', 'e', 'W', 'a', 'r', 's'], 4)).to.eql([
+            'e',
+            's',
+            'W',
+            'o',
+            'C',
+            'd',
+            'r',
+            'a',
+        ]);
         expect(josephus([1, 2, 3, 4, 5, 6, 7], 3)).to.eql([
             3, 6, 2, 7, 5, 1, 4,
         ]);
